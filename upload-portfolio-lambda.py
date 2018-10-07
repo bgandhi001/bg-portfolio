07ee931bc071a78339324fbc036b78b5efcaf96c
Bhavik Gandhi <bhavik.gandhi@me.com>
2018-10-07 01:26:45 -0400

    Update portfolio deployment lambda script with try and except

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -7,20 +7,30 @@ import mimetypes
 
 def lambda_handler(event, context):
 
-    s3 = boto3.resource('s3',config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:491462011685:deployPortfolioTopic')
 
-    portfolio_bucket = s3.Bucket('portfolio.bhavikgandhi.info')
-    build_bucket = s3.Bucket('portfoliobuild.bhavikgandhi.info')
+    try:
+        s3 = boto3.resource('s3',config=Config(signature_version='s3v4'))
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        portfolio_bucket = s3.Bucket('portfolio.bhavikgandhi.info')
+        build_bucket = s3.Bucket('portfoliobuild.bhavikgandhi.info')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-              ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                  ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print "Job Done!"
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio Deployed Successfully !")
+    except:
+         topic.publish(Subject="Portfolio Deploy Failed", Message="Portfolio was not deployed - An Error Occured !")
+         raise
 
     return {
         "statusCode": 200,
