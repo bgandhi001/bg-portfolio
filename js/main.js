15bbebdb2bcac6ba8d9b9d48c7c7ab51807b6624
Bhavik Gandhi <bhavik.gandhi@me.com>
2018-10-18 14:48:58 -0400

    New images added with Demo Example

--- js/main.js
+++ js/main.js
@@ -4,35 +4,46 @@ import ExampleWork from './example-work';
 
 const myWork = [
   {
-    'title': "AWS Cloud Project Example",
+    'title': "AWS Cloud Projects",
     'href': "BhavikGandhi_CloudProjectWork.pdf",
-    'desc': "Professional Engineer with over 10+ years of experience ranging across numerous Technology fields such as build release management, software configuration, design, development and cloud implementation, with drive to learn new things",
+    'desc': "Professional Engineer with over 10+ years of experience ranging across numerous Technology fields such as build release management, software configuration, design, development and cloud implementation, with drive to learn new things.",
     'image': {
       'desc': "example screenshot of a project involving code",
-      'src': "images/example1.png",
+      'src': "images/antelope.png",
       'comment': ""
     }
   },
   {
-    'title': "Prod-DR work Example",
+    'title': "AWS Cloud Demo",
+    'href': "BhavikGandhi_AWSCloudDemo.pdf",
+    'desc': "Example Demo work for AWS Cloud Projects",
+    'image': {
+      'desc': "example screenshot of a project involving code",
+      'src': "images/car.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Prod-DR Work",
     'href': "https://example.com",
     'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "example screenshot of a project involving chemistry",
-      'src': "images/srdf.png",
+      'src': "images/balloon.png",
       'comment': ""
     }
   },
   {
-    'title': "Migration Example",
+    'title': "Migration Work",
     'href': "https://example.com",
     'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "example screenshot of a project involving cats",
-      'src': "images/example3.png",
+      'src': "images/cheetah.png",
       'comment': `“Bengal cat” by roberto shabs is licensed under CC BY 2.0
            https://www.flickr.com/photos/37287295@N00/2540855181`
     }
-  }
+  },
+
 ]
 ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
